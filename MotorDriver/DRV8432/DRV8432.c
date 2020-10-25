1901f9a4cc1db77fe5aa55e47ab07e884cd548a2
studyHooligen <2373180028@qq.com>
2020-10-25 15:00:44 +0800

    完善直流电机双全桥驱动器的驱动文件注释

--- MotorDriver/DRV8432/DRV8432.c
+++ MotorDriver/DRV8432/DRV8432.c
@@ -1,45 +1,29 @@
+/* 
+ * DRV8432.c - The C source file of the dual H-Bridge (DRV8432) driver
+ * NOTE: This file is based on HAL library of stm32 platform
+ *      1. Read in utf8 encoding mode！
+ *      2. 本驱动程序采用差分驱动
+ *      3. 注意8432的最高驱动频率为500KHz，以及高电平保持时间
+ * 		4. 该文件理论上不需要用户修改，适配过程在头文件进行
+ *
+ * Copyright (c) 2020-, FOSH Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author            Notes           mail
+ * 2020-10-25     江榕煜     first version   2373180028@qq.com
+**/
 #include"DRV8432.h"
-#include"main.h"
 
-//PWM速度设置
-void set8432Speed_AB(float duty)
-{
-	if(duty>PWM_maxDuty) duty = PWM_maxDuty;  //防止占空比过大使得自举电路无法工作
-	else if(-duty > PWM_maxDuty) duty = -PWM_maxDuty;
-	
-	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_A_channel,PWM_cycleCount/2+(duty/200*PWM_cycleCount));
-	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_B_channel,PWM_cycleCount/2-(duty/200*PWM_cycleCount));
-	return;
-	
-}
-
-void set8432Speed_CD(float duty)
-{
-	if(duty>PWM_maxDuty) duty = PWM_maxDuty;  //防止占空比过大使得自举电路无法工作
-	else if(-duty > PWM_maxDuty) duty = -PWM_maxDuty;
-	
-	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_C_channel,PWM_cycleCount/2+(duty/200*PWM_cycleCount));
-	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_D_channel,PWM_cycleCount/2-(duty/200*PWM_cycleCount));
-	return;
-}
-
-//刹车
-void lock()
-{
-	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_A_channel,PWM_cycleCount/2);
-	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_B_channel,PWM_cycleCount/2);
-	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_C_channel,PWM_cycleCount/2);
-	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_D_channel,PWM_cycleCount/2);
-}
-
-//滑行
-void slide()
-{
-	HAL_GPIO_WritePin(DRV8432_RST_AB_Port, DRV8432_RST_AB_PIN, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(DRV8432_RST_CD_Port, DRV8432_RST_CD_PIN, GPIO_PIN_RESET);
-}
-
-//初始化
+/**
+ * @brief   初始化驱动芯片
+ * @note    调用该函数前必须已经初始化了PWM外设
+ * @param    
+ *          None
+ * @retval   
+ *          None
+**/
 void init8432()
 {
 	//关闭PWM
@@ -65,4 +49,71 @@ void init8432()
 	HAL_GPIO_WritePin(DRV8432_RST_AB_Port, DRV8432_RST_AB_PIN, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(DRV8432_RST_CD_Port, DRV8432_RST_CD_PIN, GPIO_PIN_SET);
 	
+}
+
+/**
+ * @brief   设置AB相全桥输出PWM占空比速度
+ * @note    占空比范围：-99.99% ~ +99.99%
+ * @param
+ *          duty(float)：占空比
+ * @retval
+ *          None
+**/
+void set8432Speed_AB(float duty)
+{
+	if(duty>PWM_maxDuty) duty = PWM_maxDuty;  //防止占空比过大使得自举电路无法工作
+	else if(-duty > PWM_maxDuty) duty = -PWM_maxDuty;
+	
+	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_A_channel,PWM_cycleCount/2+(duty/200*PWM_cycleCount));
+	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_B_channel,PWM_cycleCount/2-(duty/200*PWM_cycleCount));
+	return;
+	
+}
+
+/**
+ * @brief   设置CD相全桥输出PWM占空比/速度
+ * @note    占空比范围：-99.99% ~ +99.99%
+ * @param
+ *          duty(float)：占空比
+ * @retval
+ *          None
+**/
+void set8432Speed_CD(float duty)
+{
+	if(duty>PWM_maxDuty) duty = PWM_maxDuty;  //防止占空比过大使得自举电路无法工作
+	else if(-duty > PWM_maxDuty) duty = -PWM_maxDuty;
+	
+	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_C_channel,PWM_cycleCount/2+(duty/200*PWM_cycleCount));
+	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_D_channel,PWM_cycleCount/2-(duty/200*PWM_cycleCount));
+	return;
+}
+
+/**
+ * @brief   刹车
+ * @note    相间连接（反向电动势刹车）
+ * @param
+ *          None
+ * @retval
+ *          None
+**/
+void lock()
+{
+	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_A_channel,PWM_cycleCount/2);
+	__HAL_TIM_SetCompare(&DRV8432_AB_Timer,DRV8432_B_channel,PWM_cycleCount/2);
+	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_C_channel,PWM_cycleCount/2);
+	__HAL_TIM_SetCompare(&DRV8432_CD_Timer,DRV8432_D_channel,PWM_cycleCount/2);
+}
+
+/**
+ * @brief   滑行
+ * @note    关闭全桥驱动（断开相间）
+ * @param
+ *          None
+ * @retval
+ *          None
+**/
+void slide()
+{
+	HAL_GPIO_WritePin(DRV8432_RST_AB_Port, DRV8432_RST_AB_PIN, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DRV8432_RST_CD_Port, DRV8432_RST_CD_PIN, GPIO_PIN_RESET);
 }
--- MotorDriver/DRV8432/DRV8432.h
+++ MotorDriver/DRV8432/DRV8432.h
@@ -1,13 +1,31 @@
+/* 
+ * DRV8432.h - The C head file of the dual H-Bridge (DRV8432) driver
+ * NOTE: This file is based on HAL library of stm32 platform
+ *      1. Read in utf8 encoding mode！
+ *      2. 本驱动程序采用差分驱动
+ *      3. 注意8432的最高驱动频率为500KHz，以及高电平保持时间
+ *
+ * Copyright (c) 2020-, FOSH Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author            Notes           mail
+ * 2020-10-25     江榕煜     first version   2373180028@qq.com
+**/
 #ifndef DRV8432_H
 #define DRV8432_H
 
 #include "main.h"
 
-/*-----NOTICE--------
-1. Read in utf8 encoding mode！
-2. 本驱动程序采用差分驱动，注意8432的最高驱动频率为500KHz，以及高电平保持时间
-3. 使用本驱动库前需要移植以下几个参数
---------------------*/
+/*********NOTICE*********
+ * 使用前请适配以下参数：
+ * 1. PWM计数周期
+ * 2. 最大占空比
+ * 3. PWM通道定义（HAL对象）
+ * 4. 控制GPIO口定义（HAL对象）
+ * 如果不会请联系：2373180028@qq.com
+******* NOTICE-END*******/
 
 //采用三角计数模式，PWM模式一输出
 #define timer_countFrequency 240000000  //240M
@@ -30,19 +48,62 @@ extern TIM_HandleTypeDef htim5;
 #define DRV8432_RST_AB_PIN RST_AB_Pin
 #define DRV8432_RST_CD_PIN RST_CD_Pin
 
+/*********NOTICE*********
+ * 接下来是你可用的封装接口：
+ * 1. 设备初始化
+ * 2. 全桥输出占空比设置
+ * 3. 刹车
+ * 4. 滑行
+******* NOTICE-END*******/
 
-//PWM速度设置（占空比在-99.99 ~ +99.99）
-void set8432Speed_AB(float duty);
-void set8432Speed_CD(float duty);
-
-//刹车
-void lock(void);
-
-//滑行
-void slide(void);
-
-//初始化
+/**
+ * @brief   初始化驱动芯片
+ * @note    调用该函数前必须已经初始化了PWM外设
+ * @param    
+ *          None
+ * @retval   
+ *          None
+**/
 void init8432(void);
 
+/**
+ * @brief   设置AB相全桥输出PWM占空比速度
+ * @note    占空比范围：-99.99% ~ +99.99%
+ * @param
+ *          duty(float)：占空比
+ * @retval
+ *          None
+**/
+void set8432Speed_AB(float duty);
+
+/**
+ * @brief   设置CD相全桥输出PWM占空比/速度
+ * @note    占空比范围：-99.99% ~ +99.99%
+ * @param
+ *          duty(float)：占空比
+ * @retval
+ *          None
+**/
+void set8432Speed_CD(float duty);
+
+/**
+ * @brief   刹车
+ * @note    相间连接（反向电动势刹车）
+ * @param
+ *          None
+ * @retval
+ *          None
+**/
+void lock(void);
+
+/**
+ * @brief   滑行
+ * @note    关闭全桥驱动（断开相间）
+ * @param
+ *          None
+ * @retval
+ *          None
+**/
+void slide(void);
 
 #endif
